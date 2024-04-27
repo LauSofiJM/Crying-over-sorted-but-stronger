c445212da006aa4b3a019979aac5531d4e1d10d0
LauSofiJM <159340280+LauSofiJM@users.noreply.github.com>
2024-04-27 16:49:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+
+from shapes import triangle, rectangle
+triangle = triangle(is_regular=False, vertices=[], edges=[], inner_angles=[])
+print(triangle)
+rectangle = rectangle(is_regular=False, vertices=[], edges=[], inner_angles=[], width=0, height=0)
+print(rectangle)
